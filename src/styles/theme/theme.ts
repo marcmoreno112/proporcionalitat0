b76b91b8709c8d6cae09c85c7d402018dc9db907
Marc Moreno <marcmoreno112@gmail.com>
2023-08-04 01:14:49 +0200

    Create theme file

---
+++ src/styles/theme/theme.ts
@@ -0,0 +1,18 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: "#c31e24",
+    secondary: "#573928",
+    tertiary: "#F1D0C7",
+    tertiaryDark: "#80141B",
+    feedbackOk: "#23AD29",
+    feedbackError: "#c31e24",
+  },
+
+  fonts: {
+    primary: "'Montserrat', sans-serif",
+  },
+};
+
+export default theme;

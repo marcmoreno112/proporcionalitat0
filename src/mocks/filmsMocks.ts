df89ccce6bd0f450f84a0ddcfd804f7c2e53ef0f
Marc Moreno <marcmoreno112@gmail.com>
2023-08-04 23:21:49 +0200

    Create filmsMock

--- src/mocks/filmsMocks.ts
+++ src/mocks/filmsMocks.ts
@@ -6,3 +6,13 @@ export const filmMock: FilmData = {
     "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/b5fLqlXhypaD3A02ve5kOhmIx1I.jpg",
   releaseDate: "2023-03",
 };
+
+export const filmsMock: FilmData[] = [
+  filmMock,
+  {
+    filmTitle: "Atrapaalmas",
+    posterUrl:
+      "https://www.themoviedb.org/t/p/w1280/nxXdasJvK2wdEW5JHEOeezIzjnH.jpg",
+    releaseDate: "2023-08",
+  },
+];

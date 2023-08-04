db6205304bf84ea5c70896e6d571d88f3e7abb01
Marc Moreno <marcmoreno112@gmail.com>
2023-08-04 11:34:39 +0200

    Create HeaderStyled component

---
+++ src/components/Header/HeaderStyled.ts
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+const HeaderStyled = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: white;
+  top: 0;
+  padding: 20px;
+  position: fixed;
+  width: 100%;
+`;
+
+export default HeaderStyled;

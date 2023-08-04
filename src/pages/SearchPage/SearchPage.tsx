1c14b476c3a752b95896c8232d58806bb28d4641
Marc Moreno <marcmoreno112@gmail.com>
2023-08-04 14:25:27 +0200

    Create SearchPage

---
+++ src/pages/SearchPage/SearchPage.tsx
@@ -0,0 +1,11 @@
+import SearchPageStyled from "./SearchPageStyled";
+
+const SearchPage = (): React.ReactElement => {
+  return (
+    <SearchPageStyled>
+      <h1>Search some films</h1>
+    </SearchPageStyled>
+  );
+};
+
+export default SearchPage;

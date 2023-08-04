d5a16876f3f524e97195eb337fc7bd414b68e451
Marc Moreno <marcmoreno112@gmail.com>
2023-08-04 11:12:40 +0200

    Create LayoutStyled component

---
+++ src/components/Layout/LayoutStyled.ts
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+const LayoutStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 20px 20px;
+`;
+
+export default LayoutStyled;

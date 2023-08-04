e304e1ee145bc7466fcf55ae74c93453d2ae0d11
Marc Moreno <marcmoreno112@gmail.com>
2023-08-04 13:52:34 +0200

    Create search path

--- src/router/appRouter.tsx
+++ src/router/appRouter.tsx
@@ -1,10 +1,21 @@
-import { RouteObject, createBrowserRouter } from "react-router-dom";
+import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../components/App/App";
+import paths from "./paths";
 
 export const routes: RouteObject[] = [
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to={`${paths.search}`} replace />,
+      },
+      {
+        path: `${paths.search}`,
+        element: <App />,
+      },
+    ],
   },
 ];
 

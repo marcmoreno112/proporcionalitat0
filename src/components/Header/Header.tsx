69ab8a129d37ee717985c921ed24f9937c45da46
Marc Moreno <marcmoreno112@gmail.com>
2023-08-05 01:04:37 +0200

    Create robots.txt file

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,13 @@ import HeaderStyled from "./HeaderStyled";
 const Header = (): React.ReactElement => {
   return (
     <HeaderStyled>
-      <img alt="Films logo" src="/images/logo.svg" className="logo" />
+      <img
+        alt="Films logo"
+        src="/images/logo.svg"
+        className="logo"
+        width={1941}
+        height={520}
+      />
     </HeaderStyled>
   );
 };

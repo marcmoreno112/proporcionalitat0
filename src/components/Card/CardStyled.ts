1727b484d0353f52421b316a2ea01b92784fa435
Marc Moreno <marcmoreno112@gmail.com>
2023-08-04 16:02:59 +0200

    Create CardStyled component

---
+++ src/components/Card/CardStyled.ts
@@ -0,0 +1,12 @@
+import { styled } from "styled-components";
+
+const CardStyled = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+  font-size: 2rem;
+`;
+
+export default CardStyled;

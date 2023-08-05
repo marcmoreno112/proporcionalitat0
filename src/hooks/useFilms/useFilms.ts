449cb216f42549c099f07214436c85701d27e190
Marc Moreno <marcmoreno112@gmail.com>
2023-08-05 15:28:36 +0200

    Replace useAppSelector with a parameter

--- src/hooks/useFilms/useFilms.ts
+++ src/hooks/useFilms/useFilms.ts
@@ -1,28 +1,28 @@
 import axios from "axios";
-import { useAppSelector } from "../../store";
 import { FilmData } from "../../types";
 import { useCallback } from "react";
 
-const apiUrl = "https://api.themoviedb.org/3/search/movie";
+export const apiUrl = "https://api.themoviedb.org/3/search/movie";
 
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const useFilms = () => {
-  const { titleText } = useAppSelector((state) => state.filmsState);
-
   interface ApiResponse {
     results: FilmData[];
   }
 
-  const getFilms = useCallback(async (): Promise<ApiResponse | undefined> => {
-    const {
-      data: { results },
-    } = await axios.get<ApiResponse>(
-      `${apiUrl}?api_key=${apiKey}&query=${titleText}`
-    );
+  const getFilms = useCallback(
+    async (titleText: string): Promise<ApiResponse | undefined> => {
+      const {
+        data: { results },
+      } = await axios.get<ApiResponse>(
+        `${apiUrl}?api_key=${apiKey}&query=${titleText}`
+      );
 
-    return { results };
-  }, [titleText]);
+      return { results };
+    },
+    []
+  );
 
   return { getFilms };
 };

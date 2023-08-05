5c3051c3b67f07fd6a6182e37419cf4274dbfadc
Marc Moreno <marcmoreno112@gmail.com>
2023-08-05 18:51:50 +0200

    Test SearchPage with user typing text

--- src/pages/SearchPage/SearchPage.test.tsx
+++ src/pages/SearchPage/SearchPage.test.tsx
@@ -1,7 +1,10 @@
+import userEvent from "@testing-library/user-event";
 import { screen } from "@testing-library/react";
 import pageTitles from "../../utils/pageTitles";
 import { renderWithProviders } from "../../utils/testUtils";
 import SearchPage from "./SearchPage";
+import { mockTitleText } from "../../mocks/filmsMocks";
+import placeholders from "../../utils/placeholders";
 
 describe("Given a SearchPage page", () => {
   describe("When it is rendered", () => {
@@ -13,6 +16,24 @@ describe("Given a SearchPage page", () => {
       const title = screen.getByRole("heading", { name: expectedTitle });
 
       expect(title).toBeInTheDocument();
+    });
+  });
+  describe(`When it is rendered and the user types the text ${mockTitleText} in the search bar`, () => {
+    test("Then it should show a film with the text inside its title", async () => {
+      const inputText = mockTitleText;
+      const expectedPlaceholder = placeholders.searchInput;
+
+      renderWithProviders(<SearchPage />);
+
+      const input = screen.getByPlaceholderText(expectedPlaceholder);
+
+      await userEvent.type(input, inputText);
+
+      const expectedTitle = /mario/i;
+
+      const title = await screen.getAllByText(expectedTitle);
+
+      expect(title[0]).toBeInTheDocument();
     });
   });
 });

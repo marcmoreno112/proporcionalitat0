bc277c4569a68715e798521250f25e2dc9969734
Marc Moreno <marcmoreno112@gmail.com>
2023-08-05 02:19:36 +0200

    Test changeTitleText reducer of filmsSlice

---
+++ src/store/films/__tests__/changeTitleText.test.ts
@@ -0,0 +1,29 @@
+import {
+  changeTitleTextActionCreator,
+  filmsReducer,
+  initialFilmsState,
+} from "../filmsSlice";
+import { FilmsStateStructure } from "../types";
+
+describe("Given a changeTitleText reducer", () => {
+  describe("When it receives a 'Flash' text", () => {
+    test("Then it should return a new state with the titleText changed", () => {
+      const searchText = "Flash";
+
+      const initialTestFilmsState: FilmsStateStructure = initialFilmsState;
+
+      const expectedFilmsState: FilmsStateStructure = {
+        titleText: searchText,
+      };
+
+      const changeTitleTextAction = changeTitleTextActionCreator(searchText);
+
+      const newFilmsState = filmsReducer(
+        initialTestFilmsState,
+        changeTitleTextAction
+      );
+
+      expect(expectedFilmsState).toStrictEqual(newFilmsState);
+    });
+  });
+});

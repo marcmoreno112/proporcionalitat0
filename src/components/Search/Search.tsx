476e318a5c5ece1f919a04760c7cf928e74f7b9e
Marc Moreno <marcmoreno112@gmail.com>
2023-08-05 02:25:46 +0200

    Add dispatch of search text from Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 import placeholders from "../../utils/placeholders";
 import SearchStyled from "./SearchStyled";
+import { useDispatch } from "react-redux";
+import { changeTitleTextActionCreator } from "../../store/films/filmsSlice";
 
 const Search = (): React.ReactElement => {
   const [searchValue, setSearchValue] = useState("");
+
+  const dispatch = useDispatch();
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const searchText = event.target.value;
 
     setSearchValue(searchText);
+
+    const changeTitleTextAction = changeTitleTextActionCreator(searchText);
+
+    dispatch(changeTitleTextAction);
   };
 
   return (

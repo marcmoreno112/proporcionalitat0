4b526b21251c4608da85e6eee4a02e36d5400529
Marc Moreno <marcmoreno112@gmail.com>
2023-08-06 20:54:19 +0200

    Create mockRouterProvider and update Layout test

--- src/utils/testUtils.tsx
+++ src/utils/testUtils.tsx
@@ -6,6 +6,11 @@ import theme from "../styles/theme/theme";
 import GlobalStyle from "../styles/GlobalStyles/GlobalStyles";
 import { PropsWithChildren } from "react";
 import { render } from "@testing-library/react";
+import {
+  RouteObject,
+  RouterProvider,
+  createMemoryRouter,
+} from "react-router-dom";
 
 export const renderWithProviders = (
   ui: React.ReactElement,
@@ -32,3 +37,18 @@ export const providerWrapper = ({
 }: PropsWithChildren): React.ReactElement => (
   <Provider store={store}>{children}</Provider>
 );
+
+export const mockRouterProvider = (
+  ui: React.ReactElement
+): React.ReactElement => {
+  const routes: RouteObject[] = [
+    {
+      path: "/",
+      element: ui,
+    },
+  ];
+
+  const mockRouter = createMemoryRouter(routes);
+
+  return <RouterProvider router={mockRouter} />;
+};

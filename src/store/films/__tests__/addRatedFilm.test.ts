a694836a611bf32cccfc2967746f6aa155f74603
Marc Moreno <marcmoreno112@gmail.com>
2023-08-06 03:33:06 +0200

    Test addRatedFilm reducer

---
+++ src/store/films/__tests__/addRatedFilm.test.ts
@@ -0,0 +1,33 @@
+import { ratedFilmMock } from "../../../mocks/filmsMocks";
+import { FilmData } from "../../../types";
+import {
+  addRatedFilmActionCreator,
+  filmsReducer,
+  initialFilmsState,
+} from "../filmsSlice";
+import { FilmsStateStructure } from "../types";
+
+describe("Given an addRatedFilm reducer", () => {
+  describe("When it receives a rated film", () => {
+    test("Then it should return a new state with the rated film", () => {
+      const ratedFilm = ratedFilmMock;
+
+      const initialTestFilmsState: FilmsStateStructure = initialFilmsState;
+
+      const expectedFilmsState: FilmsStateStructure = {
+        detailFilm: {} as FilmData,
+        filmsRated: [ratedFilm],
+        titleText: "",
+      };
+
+      const addRatedFilmAction = addRatedFilmActionCreator(ratedFilm);
+
+      const newFilmsState = filmsReducer(
+        initialTestFilmsState,
+        addRatedFilmAction
+      );
+
+      expect(expectedFilmsState).toStrictEqual(newFilmsState);
+    });
+  });
+});

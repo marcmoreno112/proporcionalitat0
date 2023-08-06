e8e6f0f9cfcf1731c20e799b742bf1422b07131c
Marc Moreno <marcmoreno112@gmail.com>
2023-08-07 01:22:54 +0200

    Change searchInput placeholder text

--- src/utils/titles.ts
+++ src/utils/titles.ts
@@ -2,7 +2,7 @@ const titles = {
   searchPage: "Search some films",
   missingPosterPath: "Missing",
   searchNotFound: "Try another words. This search has no results",
-  searchInputPlaceholder: "Type a part of the film title",
+  searchInputPlaceholder: "Type a film title",
   fetchError: "We can't show the films. Try again later",
   emptyApiKey: "Remember to set your API key",
   myList: "Your rated films",

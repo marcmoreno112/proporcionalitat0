275151c436e0eb6db8f3fe029cdd271baba18aa5
Marc Moreno <marcmoreno112@gmail.com>
2023-08-07 07:16:53 +0200

    Give styles to nowPlaying rendering

--- src/pages/SearchPage/SearchPageStyled.ts
+++ src/pages/SearchPage/SearchPageStyled.ts
@@ -4,11 +4,23 @@ const SearchPageStyled = styled.main`
   width: 100%;
   display: flex;
   flex-direction: column;
+  align-items: center;
   gap: 40px;
 
   .page-title {
     font-size: 3rem;
     color: ${(props) => props.theme.colors.secondary};
+  }
+
+  .now-playing {
+    width: 350px;
+    height: 250px;
+
+    @media (min-width: 1200px) {
+      position: fixed;
+      left: 40px;
+      transform: translate(0, 40%);
+    }
   }
 `;
 

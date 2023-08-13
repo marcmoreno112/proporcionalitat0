f6d729a9e961e740a10553af6ce5cf27eb62b6bf
Marc Moreno <marcmoreno112@gmail.com>
2023-08-13 17:22:26 +0200

    Create SearchPageCard component

---
+++ src/components/SearchPageCard/SearchPageCardStyled.ts
@@ -0,0 +1,15 @@
+import { styled } from "styled-components";
+
+const SearchPageCardStyled = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+  font-size: 1.5rem;
+  color: ${(props) => props.theme.colors.primary};
+  border-bottom: 1px solid;
+  padding: 10px;
+`;
+
+export default SearchPageCardStyled;

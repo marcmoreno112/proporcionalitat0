1e8ea6e80b32b19a82ae89cbda0d4d30a0982e0a
Marc Moreno <marcmoreno112@gmail.com>
2023-08-18 13:20:58 +0200

    Apply the same style to MyListCardList as SearchPageCardList

--- src/components/MyListCardList/MyListCardListStyled.ts
+++ src/components/MyListCardList/MyListCardListStyled.ts
@@ -1,9 +1,19 @@
 import { styled } from "styled-components";
 
 const MyListCardListStyled = styled.ul`
-  display: flex;
-  flex-direction: column;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  align-items: center;
   gap: 40px;
+
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
 `;
 
 export default MyListCardListStyled;

ef1ad0323406309ef35f62e805a78b823b4dd2c0
Marc Moreno <marcmoreno112@gmail.com>
2025-05-30 23:40:43 +0200

    Refresh button

--- src/pages/MyListPage/MyListPage.tsx
+++ src/pages/MyListPage/MyListPage.tsx
@@ -5,8 +5,33 @@ import titles from "../../utils/titles";
 import MyListPageStyled from "./MyListPageStyled";
 import ScrollToTopButton from "../../components/ScrollToTopButton/ScrollToTopButton";
 import Question from "../../components/MyListCardList/Question";
+import styled from "styled-components";
 
 const MyListPage = (): React.ReactElement => {
+  const RefreshButton = styled.button`
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    width: 90px;
+    height: 90px;
+    border-radius: 50%;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    font-size: 24px;
+    cursor: pointer;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    transition: background-color 0.3s;
+
+    &:hover {
+      background-color: #0056b3;
+    }
+  `;
+
+  const refreshPage = () => {
+    window.location.reload();
+  };
+
   const { filmsRated } = useAppSelector((state) => state.proporState);
 
   const dispatch = useAppDispatch();
@@ -43,6 +68,8 @@ const MyListPage = (): React.ReactElement => {
       )}
 
       <Question films={filmsRated} />
+
+      <RefreshButton onClick={refreshPage}>OTRO!</RefreshButton>
     </MyListPageStyled>
   );
 };
